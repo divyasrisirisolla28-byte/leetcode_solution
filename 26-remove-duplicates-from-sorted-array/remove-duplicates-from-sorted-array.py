da5284a6f7baf03c38c162f2a1e9b4a7f44f9ae6
divyasrisirisolla28-byte <divyasrisirisolla28@gmail.com>
2026-09-01 00:19:24 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (17.60%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        unique = list(set(nums))
+        unique.sort()
+        for i in range(len(unique)):
+            nums[i] = unique[i]
+        return len(unique)
+ 
